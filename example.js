4d576321aabcb992393c50bf33e37b7214dc0d0f
mster <michaelsterpka@gmail.com>
2020-07-14 19:12:29 -0700

    add recursive init for return sheets, trim and format scraped data

--- example.js
+++ example.js
@@ -2,21 +2,25 @@
 
 const path = require('path')
 
-let DATA_SHEET = [
+const DATA_SHEET = [
   { '.b-product_name': 'plantName' },
-  { '.b-product_description': 'description' }
+  { '.b-product_description': 'description' },
+  { '.b-product_properties-property_value': 'properties' },
+  { '.b-product_growing_info-sow_info': 'health.sow' },
+  { '.b-product_growing_info-grow_info': 'health.grow' },
+  { '.b-product_growing_info-growing_tips': 'health.tips' }
 ]
 
-let URLS = [
+const URLS = [
   'https://www.burpee.com/vegetables/arugula/arugula-red-dragon-prod500402.html',
   'https://www.burpee.com/vegetables/artichokes/artichoke-imperial-star-prod000566.html'
 ]
 
 ;(async function () {
-  let ws = require('./index')
-  let opts = { write: path.join(__dirname, 'scrapes/') }
+  const WebScraper = require('./index')
+  const opts = { write: path.join(__dirname, 'scrapes/') }
 
-  let scraper = new ws(DATA_SHEET)
-  let data = await scraper.scrape(URLS)
+  const scraper = new WebScraper(DATA_SHEET, opts)
+  const data = await scraper.scrape(URLS)
   console.log(data)
 })()
